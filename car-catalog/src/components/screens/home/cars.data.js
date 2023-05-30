3d07dc05a5b545bbec4c6b4a2ec28095b1e0f0fc
Egor Ovcharov <92940275+G0RYNYCH@users.noreply.github.com>
2023-05-30 20:58:32 +0300

    add cycle

---
+++ car-catalog/src/components/screens/home/cars.data.js
@@ -0,0 +1,14 @@
+ export const cars = [
+    {
+        id: 1,
+        name: 'Toyota Camry',
+        price: 100000,
+        image: 'https://images.pexels.com/photos/11285174/pexels-photo-11285174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+        id: 2,
+        name: 'Honda Civic',
+        price: 100000,
+        image: 'https://images.pexels.com/photos/6794815/pexels-photo-6794815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+]
